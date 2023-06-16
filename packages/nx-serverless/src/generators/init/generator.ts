a5cd11d24f2af294b7ccf51007866613654b153f
Andres Manjarres <andres.manjarres@trades.org>
2023-06-16 11:09:03 -0500

    fix: apply release only for serverless package

--- packages/nx-serverless/src/generators/init/generator.ts
+++ packages/nx-serverless/src/generators/init/generator.ts
@@ -1,5 +1,4 @@
 import {
-  addDependenciesToPackageJson,
   formatFiles,
   GeneratorCallback,
   readWorkspaceConfiguration,
@@ -9,7 +8,6 @@ import {
 import { jestInitGenerator } from '@nrwl/jest';
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import { setDefaultCollection } from '@nrwl/workspace/src/utilities/set-default-collection';
-import { devDependencies } from '@trades-org/nx-core';
 import { InitGeneratorSchema } from './schema';
 
 export default async function serverlessInitGenerator(host: Tree, options: InitGeneratorSchema) {
@@ -24,30 +22,11 @@ export default async function serverlessInitGenerator(host: Tree, options: InitG
     tasks.push(jestTask);
   }
 
-  // Disable updating root monorepo dependencies
-  // const installTask = updateDependencies(host, options);
-  // tasks.push(installTask);
-
   if (!options.skipFormat) {
     await formatFiles(host);
   }
 
   return runTasksInSerial(...tasks);
-}
-
-function updateDependencies(host: Tree, options: InitGeneratorSchema) {
-  return addDependenciesToPackageJson(
-    host,
-    {},
-    {
-      '@trades-org/nx-serverless': '*',
-      serverless: devDependencies['serverless'],
-      'serverless-offline': devDependencies['serverless-offline'],
-      ...(options.plugin === 'serverless-bundle'
-        ? { '@trades-org/serverless-bundle': devDependencies['@trades-org/serverless-bundle'] }
-        : { '@nrwl/node': '*' }),
-    },
-  );
 }
 
 function updateGitignore(host: Tree) {
