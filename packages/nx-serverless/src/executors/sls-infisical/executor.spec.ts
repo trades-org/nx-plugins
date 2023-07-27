1c69f1d90de32ffe34620b3ecfed3a76c5ac6ab9
Andres Manjarres <andres.manjarres@trades.org>
2023-07-27 08:18:06 -0500

    fix: fix eslint issue

--- packages/nx-serverless/src/executors/sls-infisical/executor.spec.ts
+++ packages/nx-serverless/src/executors/sls-infisical/executor.spec.ts
@@ -1,11 +1,16 @@
-import { SlsInfisicalExecutorSchema } from './schema';
+import { testContext } from '../../utils/test-context';
 import executor from './executor';
+import { SlsInfisicalExecutorSchema } from './schema';
 
-const options: SlsInfisicalExecutorSchema = {};
+const options: SlsInfisicalExecutorSchema = {
+  command: '',
+  infisicalPath: '',
+  infisicalEnv: '',
+};
 
 describe('SlsInfisical Executor', () => {
   it('can run', async () => {
-    const output = await executor(options);
+    const output = await executor(options, testContext);
     expect(output.success).toBe(true);
   });
 });
